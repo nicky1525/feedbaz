ab644b330a8376dab7cd052a610a71a059804bbf
nicky1525 <nicky_1525@hotmail.it>
2015-03-22 22:40:57 +0000

    Add landingviewcontroller

---
+++ LandingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LandingViewController.swift
+//  feedbaz
+//
+//  Created by Mockingjay on 22/03/2015.
+//  Copyright (c) 2015 nicky1525. All rights reserved.
+//
+
+import UIkit
+
+class LandingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

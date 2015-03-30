a026f0b595db3082c350c2da1b1c614aca42f28e
Nicole De La Feld <nicole.delafeld@dxi.eu>
2015-03-30 15:46:16 +0100

    fix autolayout constraints

---
+++ feedbaz/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  feedbaz
+//
+//  Created by Nicole De La Feld on 30/03/2015.
+//  Copyright (c) 2015 nicky1525. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
